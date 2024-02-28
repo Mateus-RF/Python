9be97cd433d6b5f796911f70e22dba58643e36d9
Mateus Ribeiro Ferreira <90785350+Mateus-RF@users.noreply.github.com>
2024-02-28 18:01:10 -0300

    update

---
+++ Curso Udemy/Exercicios/Ex2.py
@@ -0,0 +1,12 @@
+nome = 'Mateus Ribeiro'
+altura = 1.74
+peso = 75
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+# Mateus Ribeiro tem 1.74 de altura,
+# pesa 75 quilos e seu IMC é
+# 24.772096710265558
---
+++ Curso Udemy/Exercicios/Ex3.py
@@ -0,0 +1,12 @@
+# Exercício de programação com if e comparação
+Primeiro_Valor = input('Digite um numero: ')
+Segundo_Valor = input('Digite outro numero: ')
+
+if Primeiro_Valor >= Segundo_Valor:
+    print(
+        f'{Primeiro_Valor=} é maior ou igual do que {Segundo_Valor=}'
+        )
+else:
+    print(
+        f'{Segundo_Valor=} é maior ou igual do que {Primeiro_Valor=}'
+        )
