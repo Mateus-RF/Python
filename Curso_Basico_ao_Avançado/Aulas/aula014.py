0bf46e07c4fd38f8634a0551b43d042d2e6a3efd
Mateus Ribeiro Ferreira <90785350+Mateus-RF@users.noreply.github.com>
2024-03-17 18:44:13 -0300

    update

---
+++ Curso_Basico_ao_Avançado/Aulas/aula014.py
@@ -0,0 +1,8 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'tipos imnutaveis'
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+print(string)
+print(outra_variavel)
