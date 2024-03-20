92308c8b0b250c24ec69d0b81771d2171435d47d
Mateus Ribeiro Ferreira <90785350+Mateus-RF@users.noreply.github.com>
2024-03-19 21:28:35 -0300

    .

---
+++ Curso_Basico_ao_Avançado/Exercicios/Ex008.py
@@ -0,0 +1,28 @@
+frase = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
+'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+'Nunc aliquet bibendum enim facilisis gravida. Cursus risus at ultrices mi tempus.'
+
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
+while i < len(frase):
+    letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
+        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+    i += 1
+
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_apareceu_mais_vezes}" que apareceu '
+    f'{qtd_apareceu_mais_vezes}x'
+)
